1b8fcd0303c3d9df2f12a718eac85588630ad46a
Mrbysco <Mrbysco@users.noreply.github.com>
2022-03-08 04:17:50 +0100

    Setup workflow [push]
    Insert a config option to make the ticking random

--- src/main/java/com/mrbysco/ignition/blocks/CustomWallTorchBlock.java
+++ src/main/java/com/mrbysco/ignition/blocks/CustomWallTorchBlock.java
@@ -1,5 +1,6 @@
 package com.mrbysco.ignition.blocks;
 
+import com.mrbysco.ignition.config.IgnitionConfig;
 import com.mrbysco.ignition.util.FlammabilityUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -19,14 +20,21 @@ public class CustomWallTorchBlock extends WallTorchBlock {
 	}
 
 	@Override
+	public boolean isRandomlyTicking(BlockState state) {
+		return IgnitionConfig.COMMON.randomTicking.get();
+	}
+
+	@Override
 	public void tick(BlockState state, ServerLevel level, BlockPos pos, Random random) {
-		level.scheduleTick(pos, this, FlammabilityUtil.getFireTickDelay(level.random));
+		if (!IgnitionConfig.COMMON.randomTicking.get()) {
+			level.scheduleTick(pos, this, FlammabilityUtil.getFireTickDelay(level.random));
+		}
 		if (level.getGameRules().getBoolean(GameRules.RULE_DOFIRETICK)) {
 			int i = random.nextInt(3);
 			if (i > 0) {
 				BlockPos blockpos = pos;
 
-				for(int j = 0; j < i; ++j) {
+				for (int j = 0; j < i; ++j) {
 					blockpos = blockpos.offset(random.nextInt(3) - 1, 1, random.nextInt(3) - 1);
 					if (!level.isLoaded(blockpos)) {
 						return;
@@ -43,7 +51,7 @@ public class CustomWallTorchBlock extends WallTorchBlock {
 					}
 				}
 			} else {
-				for(int k = 0; k < 3; ++k) {
+				for (int k = 0; k < 3; ++k) {
 					BlockPos offsetPos = pos.offset(random.nextInt(3) - 1, 0, random.nextInt(3) - 1);
 					if (!level.isLoaded(offsetPos)) {
 						return;
@@ -61,6 +69,8 @@ public class CustomWallTorchBlock extends WallTorchBlock {
 	@Override
 	public void onPlace(BlockState state, Level level, BlockPos pos, BlockState oldState, boolean isMoving) {
 		super.onPlace(state, level, pos, oldState, isMoving);
-		level.scheduleTick(pos, this, FlammabilityUtil.getFireTickDelay(level.random));
+		if (!IgnitionConfig.COMMON.randomTicking.get()) {
+			level.scheduleTick(pos, this, FlammabilityUtil.getFireTickDelay(level.random));
+		}
 	}
 }
--- src/main/java/com/mrbysco/ignition/config/IgnitionConfig.java
+++ src/main/java/com/mrbysco/ignition/config/IgnitionConfig.java
@@ -1,12 +1,14 @@
 package com.mrbysco.ignition.config;
 
 import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
 import net.minecraftforge.common.ForgeConfigSpec.IntValue;
 import org.apache.commons.lang3.tuple.Pair;
 
 public class IgnitionConfig {
 	public static class Common {
 		public final IntValue torchTickDelay;
+		public final BooleanValue randomTicking;
 
 		Common(ForgeConfigSpec.Builder builder) {
 			builder.comment("General settings")
@@ -15,6 +17,10 @@ public class IgnitionConfig {
 			torchTickDelay = builder
 					.comment("The delay between torch ticks [Default: 60]")
 					.defineInRange("torchTickDelay", 60, 1, Integer.MAX_VALUE);
+
+			randomTicking = builder
+					.comment("Make the ticking completely random [Default: false]")
+					.define("randomTicking", false);
 
 			builder.pop();
 		}
