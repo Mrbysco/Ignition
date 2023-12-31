5b67530faff2a0ef25ed8d9250db754937f83501
Mrbysco <Mrbysco@users.noreply.github.com>
2023-12-31 03:39:56 +0100

    Port to 1.20.4

--- src/main/java/com/mrbysco/ignition/Ignition.java
+++ src/main/java/com/mrbysco/ignition/Ignition.java
@@ -6,7 +6,6 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 @Mod(Ignition.MOD_ID)
@@ -14,8 +13,7 @@ public class Ignition {
 	public static final String MOD_ID = "ignition";
 	public static final Logger LOGGER = LogUtils.getLogger();
 
-	public Ignition() {
-		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+	public Ignition(IEventBus eventBus) {
 		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, IgnitionConfig.commonSpec);
 		eventBus.register(IgnitionConfig.class);
 	}
