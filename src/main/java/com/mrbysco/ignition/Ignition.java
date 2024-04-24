b2d55f28bea65945b30a83a94d5fb7b408489049
Mrbysco <Mrbysco@users.noreply.github.com>
2024-04-24 22:58:41 +0200

    Port to 1.20.5

--- src/main/java/com/mrbysco/ignition/Ignition.java
+++ src/main/java/com/mrbysco/ignition/Ignition.java
@@ -2,7 +2,9 @@ package com.mrbysco.ignition;
 
 import com.mojang.logging.LogUtils;
 import com.mrbysco.ignition.config.IgnitionConfig;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
@@ -13,8 +15,8 @@ public class Ignition {
 	public static final String MOD_ID = "ignition";
 	public static final Logger LOGGER = LogUtils.getLogger();
 
-	public Ignition(IEventBus eventBus) {
-		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, IgnitionConfig.commonSpec);
+	public Ignition(IEventBus eventBus, Dist dist, ModContainer container) {
+		container.registerConfig(ModConfig.Type.COMMON, IgnitionConfig.commonSpec);
 		eventBus.register(IgnitionConfig.class);
 	}
 }
