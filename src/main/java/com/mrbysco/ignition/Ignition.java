9781b6513a94d14e38877535c13a440ede06a768
Mrbysco <Mrbysco@users.noreply.github.com>
2025-11-05 23:41:09 +0100

    Enable in-game config

--- src/main/java/com/mrbysco/ignition/Ignition.java
+++ src/main/java/com/mrbysco/ignition/Ignition.java
@@ -2,10 +2,13 @@ package com.mrbysco.ignition;
 
 import com.mojang.logging.LogUtils;
 import com.mrbysco.ignition.config.IgnitionConfig;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import org.slf4j.Logger;
 
 @Mod(Ignition.MOD_ID)
@@ -13,8 +16,12 @@ public class Ignition {
 	public static final String MOD_ID = "ignition";
 	public static final Logger LOGGER = LogUtils.getLogger();
 
-	public Ignition(IEventBus eventBus, ModContainer container) {
+	public Ignition(IEventBus eventBus, ModContainer container, Dist dist) {
 		container.registerConfig(ModConfig.Type.COMMON, IgnitionConfig.commonSpec);
 		eventBus.register(IgnitionConfig.class);
+
+		if (dist.isClient()) {
+			container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+		}
 	}
 }
